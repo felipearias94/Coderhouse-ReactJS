f5856e3b1463ebf337191d69e746e225998b15a6
Felipe Arias <felari94@gmail.com>
2023-03-30 21:36:28 -0300

    loader agregado

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -9,6 +9,6 @@ export const getProducts = () => {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve(products);
-        }, 3000);
+        }, 5000);
     })
 }
--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -2,21 +2,26 @@ import { useEffect, useState } from "react";
 import { getProducts } from "../../asyncmock";
 import ItemCounter from "../ItemCounter/ItemCounter";
 import ItemList from "../ItemList/ItemList";
+import Loader from "../shared/Loader/Loader";
 import "./ItemListContainer.css";
 
 function ItemListContainer({ greetings }) {
 	const [products, setProducts] = useState([]);
+	const [isLoaded, setIsLoaded] = useState(true);
 
 	useEffect(() => {
 		getProducts()
 			.then((response) => {
+				setIsLoaded(false);
 				setProducts(response);
 			})
-			.catch((error) => console.error(error));
+			.catch((error) => console.error(error))
+			.finally(setIsLoaded(true));
 	}, []);
 
 	return (
 		<div>
+			<Loader isLoaded={isLoaded} />
 			<h1 className='title'>{greetings}</h1>
 			<ItemList products={products} />
 		</div>
