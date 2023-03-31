7cb4ef12a0abce17db7bb69116391887410b5a46
Felipe Arias <felari94@gmail.com>
2023-03-31 11:07:04 -0300

    fuentes movidas dentro de assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import ItemListContainer from "./components/itemListContainer/ItemListContainer";
 import NavBar from "./components/navbar/NavBar";
-import "./fonts/Breathing.ttf";
-import "./fonts/Blackpast.ttf";
-import "./fonts/Swiss721.ttf";
+import "./components/assets/fonts/Breathing.ttf";
+import "./components/assets/fonts/Blackpast.ttf";
+import "./components/assets/fonts/Swiss721.ttf";
 
 function App() {
 	return (
